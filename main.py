fc3e61f366c3811b5e25c8ec67006758c9c6eeb5
Decolour <decolourrr@gmail.com>
2024-06-06 02:49:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -10,7 +11,14 @@ running = True
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption('Игра Тир')
-icon = pygame.image.load(/images/pngwing.com.png)
+icon = pygame.image.load('images/pngwing.com.png')
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load('images/')
+target_width = 50
+target_height = 50
+
+target_x =
 
 while running:
     pass
