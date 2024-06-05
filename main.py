0c91d9c2b7da68a4b98cc3f7de1e9282d76fd769
Decolour <decolourrr@gmail.com>
2024-06-06 02:57:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ target_height = 50
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+
 while running:
     pass
 
